c1b1ad439b1c05abe3fec60942ac23a04f6c0ebe
Prophydr <155489466+Prophydr@users.noreply.github.com>
2024-04-01 08:39:47 +0200

    base set up

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
-mod card;
-use card::{Card, Suits};
+mod shoe;
+use shoe::shoe::Shoe;
+// use shoe::shoe::Shoe;
+
+// mod card;
+// mod shoe;
+// use card::{Card, Suits};
+// mod card;
+// use shoe::shoe::Shoe;
 
 
 // use core::str;
 // use std::string::ToString;
-use strum::IntoEnumIterator;
+// use strum::IntoEnumIterator;
 // use strum_macros::EnumIter; 
 
 
 fn main() {
-    let suit_size = 14;
 
-    let mut deck: Vec<Card> = Vec::new();
+    let shoe: Shoe = Shoe::new(1);
 
-    for suit in Suits::iter() {
+    // let suit_size = 14;
 
-        for i in 1 .. suit_size {
-            deck.push(Card::new(suit, i));
-        }
-    }
+    // let mut deck: Vec<Card> = Vec::new();
+
+    // for suit in Suits::iter() {
+
+    //     for i in 1 .. suit_size {
+    //         deck.push(Card::new(suit, i));
+    //     }
+    // }
 
 
-    for card in deck  {
-        println!("Card: {0}", card.name);
-        println!("Suit: {0}", card.suit);
-        println!("Value: {0}", card.number);
-        println!("Unicode: {0}", card.unicode);
-    }
+    // for card in deck  {
+    //     println!("Card: {0}", card.name);
+    //     println!("Suit: {0}", card.suit);
+    //     println!("Value: {0}", card.number);
+    //     println!("Unicode: {0}", card.unicode);
+    // }
         
 }
--- src/shoe.rs
+++ src/shoe.rs
@@ -1,6 +1,22 @@
-
+mod card;
 
 pub mod shoe {
+    use super::card::card::Card;
 
+    pub struct Shoe {
+        number_of_decks: u8,
+        decks: Vec<Card>,
+    }
 
+    impl Shoe {
+        pub fn new(number_of_decks: u8) -> Self {
+            Self {
+                number_of_decks,
+                decks: Self::create_decks(number_of_decks),
+            }
+        }
+        fn create_decks(number_of_decks: u8) -> Vec<Card> {
+            todo!()
+        }
+    }
 }
